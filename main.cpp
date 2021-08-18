73d0e5d9577cf5b966f19039674d2019fbe49944
Михаил <69348201+13MrBlackCat13@users.noreply.github.com>
2021-08-18 12:29:26 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//простой калькулятор
+int main() {
+    int a;
+    int b;
+    //int c;
+    cout << "Введите число которые вы хотите сложить. \n";
+    cin >> a;
+    cout << " Введите другое число которое вы хотите сложить. \n";
+    cin >> b;
+    cout << a + b;
+    return 0;
+}
